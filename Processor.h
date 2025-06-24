a9d9c853ebd427127f208dfff47ae8cd8c3cf44e
Padinant <123909022+Padinant@users.noreply.github.com>
2025-06-24 00:28:34 -0400

    Added a few function prototypes to Processor.h
    Changes:
    - Added prototypes for 2 pipeline-related functions: instructionDecode, getDependencies
    - Added prototypes for 2 memory-related functions: load, store
    - Added a memory member attribute (although all memory spaces are currently filled with zeros)
    - minor fix

--- Processor.h
+++ Processor.h
@@ -13,6 +13,7 @@ using namespace std;
  
 class Processor{
 public:
+
   // Constructor
   // Preconditions: None
   // Postconditions: None
@@ -32,9 +33,46 @@ public:
   // Preconditions: filename contains valid instructions
   void loadInstructions(string filename);
 
+
+  // MEMORY FUNCTIONS:
+
+  // NEW (not yet in Processor.cpp)
+  // loads data from memory into register
+  void load(string register_address, string memory_address);
+  
+  // NEW (not yet in Processor.cpp)
+  // loads data from register into memory
+  void store(string register_address, string memory_address);
+
+
+
+  // PIPELINING IMPLEMENTATION FUNCTIONS:
+
+  // NEW (not yet in Processor.cpp)
+  // decode a given instruction, (this would correspond to the ID stage)
+  // would interpret the instruction type, its dependencies, and other relevant details
+  void instructionDecode(Instruction x);
+
+  // NEW (not yet in Processor.cpp)
+  // finds the names of the dependencies for a given instruction (is helper to instructionDecode())
+  // todo: implement a way to return the dependencies (either member attributes, or return a list of register names)
+  void getDependencies(Instruction x);
+
+
+
+
+  // DISPLAY RELATED FUNCTIONS:
+  // TODO LATER
+
+
+
 private:
-  Instruction* m_instructions;
+  Instruction* m_instructions[0];
   string m_filename;
+  // NEW (not yet in Processor.cpp)
+  int memory[18] = {0};   // when initializing the processor, set as memory values from the writeup
+  //                            // OR: replace this with a global constant
+
 };
  
 #endif
